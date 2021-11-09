ff9f2edf1481c0b5156711a68f993c990d11bdc9
shital-Munemanik <93929060+shital-Munemanik@users.noreply.github.com>
2021-11-09 12:24:42 +0530

    First_Assignment

---
+++ Write a Python program that accepts a word from the user and reverse it..py
@@ -0,0 +1,12 @@
+#Write a Python program that accepts a word from the user and reverse it.
+
+
+string=input("Enter a string :")
+print(string[::-1])
+
+
+
+
+
+
+
---
+++ Write a Python program to get the Fibonacci series between 0 to 50.py
@@ -0,0 +1,19 @@
+#Fibonacci series program
+
+num1=0
+num2=1
+num3=1
+print("Fibonacci Series")
+print(num2)
+while num3<=50:
+    print(num3)
+    num1=num2
+    num2=num3
+    num3=num1+num2
+
+
+
+
+
+
+
